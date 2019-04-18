42160ab60231488595471d3e7a2bcf7904ae90fa
EC2 Default User <ec2-user@ip-172-31-22-181.us-east-2.compute.internal>
2019-04-18 19:13:54 +0000

    Change the css to allow overflow

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1425,7 +1425,7 @@ if (module.hot) {(function () {  var hotAPI = require("vue-hot-reload-api")
   if (!module.hot.data) {
     hotAPI.createRecord("data-v-1825962d", __vue__options__)
   } else {
-    hotAPI.rerender("data-v-1825962d", __vue__options__)
+    hotAPI.reload("data-v-1825962d", __vue__options__)
   }
 })()}
 });
